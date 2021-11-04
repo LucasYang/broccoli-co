855d3b79acb549df09ee0204e32766e833b43f77
LucasYang <tgyang0603@gmail.com>
2021-11-03 22:01:19 -0700

    feat: add modal logic and styling

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -1,16 +1,20 @@
 import React from "react";
+import modalStyles from './index.module.css';
 
 interface ModalInterface {
   show: boolean;
-  children: React.ReactChildren;
+  children: React.ReactElement;
   onClose: () => void;
 }
 
 export function Modal({ show, children, onClose }: ModalInterface) {
+  const showHideClassName = show ? `${modalStyles.container} ${modalStyles.displayBlock}` : `${modalStyles.container} ${modalStyles.displayNone}`;
   return (
-    <div>
-      <section className="modal-main">{children}</section>
-      <button type="button">Close</button>
+    <div className={showHideClassName}>
+      <section className={modalStyles.main}>
+        {children}
+        <button type="button" onClick={onClose}>Close</button>
+      </section>
     </div>
   );
 }
