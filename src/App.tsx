8bb1f95559d2d7c74561ad4063934993aa3c98de
LucasYang <tgyang0603@gmail.com>
2021-11-04 00:36:31 -0700

    feat: add form structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,6 @@
 import React from "react";
+import Modal from "./components/Modal";
+import Form from "./components/Form";
 import "./App.css";
 
 function App() {
@@ -14,6 +16,38 @@ function App() {
         <p>made with [love] in Melbourne.</p>
         <p>copy 2016 Broccoli & Co. All rights reserved.</p>
       </footer>
+      <Modal show onClose={() => { }}>
+        <Form name="landingPageForm">
+          {({ error, register }: any) => {
+            return (
+              <div>
+                <fieldset className="form-fieldset">
+                  <label htmlFor="fullName">
+                    Full name
+                    <input name="fullName" type="text" defaultValue="" ref={(ref) => register(ref, 'fullName')} />
+                    {error["fullName"] ? "error" : null}
+                  </label>
+                </fieldset>
+                <fieldset className="form-fieldset">
+                  <label htmlFor="email">
+                    Email
+                    <input name="email" type="email" defaultValue="" ref={(ref) => register(ref, 'email')} />
+                  </label>
+                </fieldset>
+                <fieldset className="form-fieldset">
+                  <label htmlFor="confirmEmail">
+                    Confirm email
+                    <input name="confirmEmail" type="email" defaultValue="" ref={(ref) => register(ref, 'confirmEmail')} />
+                  </label>
+                </fieldset>
+                <fieldset>
+                  <button type="submit">Send</button>
+                </fieldset>
+              </div>
+            )
+          }}
+        </Form>
+      </Modal>
     </div>
   );
 }
--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,3 +1,41 @@
-export function Form() {
-  return null;
+import React, { useRef } from 'react';
+import formStyles from './index.module.css';
+interface FormInterface {
+  name: string;
+  children: (props: any) => React.ReactElement;
 }
+
+export function Form({ children, name }: FormInterface) {
+  const childrenRef = useRef<{ [key: string]: HTMLFormElement }>({});
+
+  function submitHandler(e: React.FormEvent<HTMLFormElement>) {
+    e.preventDefault();
+    console.log(childrenRef.current);
+  }
+
+  function validateForm() {
+    // todo: validation logic
+  }
+
+  function register(ref: HTMLFormElement, name: any) {
+    childrenRef.current[name] = ref;
+  }
+
+  return (
+    <form
+      className={formStyles.container}
+      onSubmit={submitHandler}
+      noValidate
+      name={name}
+    >
+      {children({
+        loading: false,
+        error: false,
+        data: {},
+        register: register
+      })}
+    </form>
+  );
+}
+
+export default Form;
