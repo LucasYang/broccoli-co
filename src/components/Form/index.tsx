e277da1f675cd6e84a3acf6115d2faa65b99e591
LucasYang <tgyang0603@gmail.com>
2021-11-04 17:33:01 -0700

    feat: add landing form validation

--- src/components/Form/index.tsx
+++ src/components/Form/index.tsx
@@ -1,24 +1,45 @@
-import React, { useRef } from 'react';
+import React, { useRef, useState } from 'react';
 import formStyles from './index.module.css';
 interface FormInterface {
   name: string;
+  handleSubmit: (inputsValid: boolean) => any;
   children: (props: any) => React.ReactElement;
 }
 
-export function Form({ children, name }: FormInterface) {
-  const childrenRef = useRef<{ [key: string]: HTMLFormElement }>({});
+export function Form({ children, name, handleSubmit }: FormInterface) {
+  const childrenRef = useRef<{ [key: string]: any }>({});
+  const [inputErrors, setInputErrors] = useState<{ [key: string]: any }>({});
 
   function submitHandler(e: React.FormEvent<HTMLFormElement>) {
     e.preventDefault();
-    console.log(childrenRef.current);
+    const inputsValid = validateFormInputs();
+    handleSubmit(inputsValid);
   }
 
-  function validateForm() {
-    // todo: validation logic
+  function validateFormInputs() {
+    const newErrors = inputErrors;
+    for (const [key, obj] of Object.entries(childrenRef.current)) {
+      const { validationFunc, ref } = obj;
+      if (validationFunc) {
+        const { error } = validationFunc(ref.value);
+        newErrors[key] = error;
+      } else {
+        newErrors[key] = null;
+      }
+    }
+
+    setInputErrors({
+      ...newErrors,
+    });
+
+    return Object.values(newErrors).filter((e) => e !== null).length === 0;
   }
 
-  function register(ref: HTMLFormElement, name: any) {
-    childrenRef.current[name] = ref;
+  function register(ref: HTMLFormElement, name: string, validationFunc: (str: string) => any) {
+    childrenRef.current[name] = {
+      ref,
+      validationFunc,
+    };
   }
 
   return (
@@ -29,10 +50,9 @@ export function Form({ children, name }: FormInterface) {
       name={name}
     >
       {children({
+        inputErrors,
+        register,
         loading: false,
-        error: false,
-        data: {},
-        register: register
       })}
     </form>
   );
--- src/page/landing/landing__form.tsx
+++ src/page/landing/landing__form.tsx
@@ -1,31 +1,83 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import Form from "../../components/Form";
+import { isEmailValid, stringsMatch, NAME_MIN_LENGTH } from "../../utils/formValidation";
 
 export function LandingPageForm() {
+  const emailRef = useRef<HTMLInputElement | null>();
+  const confirmEmailRef = useRef<HTMLInputElement | null>();
+  const [emailsMatch, setEmailsMatch] = useState<boolean | undefined>();
+
+  const validateEmail = (str: string) => {
+    const isValid = isEmailValid(str);
+    return {
+      error: isValid ? null : 'email format incorrect',
+    }
+  };
+
+  const validateFullName = (str: string) => {
+    const isValid = str.length >= NAME_MIN_LENGTH;
+
+    return {
+      error: isValid ? null : 'Name needs to be at least 3 characters long'
+    }
+  };
+
+  const validateEmailsMatch = () => {
+    const isValid = stringsMatch(emailRef.current?.value || '', confirmEmailRef.current?.value || '');
+    return isValid;
+  }
+
+  const handleLandingPageFormSubmit = (inputsValid: boolean) => {
+    const match = validateEmailsMatch();
+    setEmailsMatch(match);
+    if (!inputsValid || !match) return;
+
+    console.log('send to server');
+  };
+
   return (
-    <Form name="landingPageForm">
-      {({ error, register }: any) => {
+    <Form name="landingPageForm" handleSubmit={handleLandingPageFormSubmit}>
+      {({ inputErrors, register }: any) => {
         // form content
         return (
           <div>
             <fieldset className="form-fieldset">
               <label htmlFor="fullName">
                 Full name
-                <input name="fullName" type="text" defaultValue="" ref={(ref) => register(ref, 'fullName')} />
-                {error["fullName"] ? "error" : null}
+                <input name="fullName" type="text" defaultValue="" ref={(ref) => register(ref, 'fullName', validateFullName)} />
               </label>
+              <div>{inputErrors["fullName"] ? "error" : null}</div>
             </fieldset>
             <fieldset className="form-fieldset">
               <label htmlFor="email">
                 Email
-                <input name="email" type="email" defaultValue="" ref={(ref) => register(ref, 'email')} />
+                <input
+                  name="email"
+                  type="email"
+                  defaultValue=""
+                  ref={(ref) => {
+                    emailRef.current = ref;
+                    register(ref, 'email', validateEmail)
+                  }}
+                />
               </label>
+              <div>{inputErrors["email"] ? "error" : null}</div>
             </fieldset>
             <fieldset className="form-fieldset">
               <label htmlFor="confirmEmail">
                 Confirm email
-                <input name="confirmEmail" type="email" defaultValue="" ref={(ref) => register(ref, 'confirmEmail')} />
+                <input
+                  name="confirmEmail"
+                  type="email"
+                  defaultValue=""
+                  ref={(ref) => {
+                    confirmEmailRef.current = ref;
+                    register(ref, 'confirmEmail', validateEmail);
+                  }}
+                />
               </label>
+              <div>{inputErrors["confirmEmail"] ? "error" : null}</div>
+              <div>{!emailsMatch === true && "emails do not match"}</div>
             </fieldset>
             <fieldset>
               <button type="submit">Send</button>
