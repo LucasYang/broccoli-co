fe211628451a86127a4849b80761feee89835627
LucasYang <tgyang0603@gmail.com>
2021-11-04 23:44:42 -0700

    feat: add Button component

---
+++ src/components/Button/index.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import styles from './index.module.css';
+
+interface ButtonInterface {
+  children: React.ReactElement;
+  className?: string;
+  onClick?: (e: React.MouseEvent<HTMLElement>) => void;
+}
+
+export function Button({ children, className, ...rest }: ButtonInterface) {
+  return (
+    <button {...rest} className={`${styles.btn} ${className}`}>{children}</button>
+  );
+}
+
+export default Button;
